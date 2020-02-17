981f2bbb42b50109fa5185ed764e235e6c328fb4
AndrLewis <lewis.andrew.byu@outlook.com>
2020-02-18 03:31:54 +0800

    LargeCardSliderComponent Init

---
+++ src/app/components/large-card-slider/large-card-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-large-card-slider',
+  templateUrl: './large-card-slider.component.html',
+  styleUrls: ['./large-card-slider.component.css']
+})
+export class LargeCardSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
