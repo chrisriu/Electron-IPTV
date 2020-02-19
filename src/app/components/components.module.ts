a20016860fa9527d84bb2835038d32e3f400dc34
AndrLewis <lewis.andrew.byu@outlook.com>
2020-02-19 06:28:00 -0500

    Remove Movie and TV, Series,Radio Slider Component.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,24 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { ResizeDiv } from './resize-div/resize-div';
 import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
-import { MultiMovieSliderComponent } from './multi-movie-slider/multi-movie-slider.component';
-import { MultiTvChannelSliderComponent } from './multi-tv-channel-slider/multi-tv-channel-slider.component';
-import { MultiSeriesSliderComponent } from './multi-series-slider/multi-series-slider.component';
-import { MultiRadioSliderComponent } from './multi-radio-slider/multi-radio-slider.component';
-import { AccountPartComponent } from './account-part/account-part.component';
-
-import { AngularResizedEventModule } from 'angular-resize-event';
 import { LargeCardSliderComponent } from './large-card-slider/large-card-slider.component';
 import { SmallCardSliderComponent } from './small-card-slider/small-card-slider.component';
+import { AccountPartComponent } from './account-part/account-part.component';
+import { AngularResizedEventModule } from 'angular-resize-event';
+
 
 @NgModule({
     declarations: [
         ResizeDiv,
         SidebarMenuComponent,
-        MultiMovieSliderComponent,
-        MultiTvChannelSliderComponent,
-        MultiSeriesSliderComponent,
-        MultiRadioSliderComponent,
         AccountPartComponent,
         LargeCardSliderComponent,
         SmallCardSliderComponent,
@@ -35,10 +27,6 @@ import { SmallCardSliderComponent } from './small-card-slider/small-card-slider.
     exports: [
         ResizeDiv,
         SidebarMenuComponent,
-        MultiMovieSliderComponent,
-        MultiTvChannelSliderComponent,
-        MultiSeriesSliderComponent,
-        MultiRadioSliderComponent,
         AccountPartComponent,
         LargeCardSliderComponent,
         SmallCardSliderComponent,
