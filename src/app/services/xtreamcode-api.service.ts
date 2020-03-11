fae13cdfe81aefa3e649c1be6baa37173df752e3
AndrLewis <lewis.andrew.byu@outlook.com>
2020-03-11 19:49:42 +0800

    Done Fix of Categories api.

--- src/app/services/xtreamcode-api.service.ts
+++ src/app/services/xtreamcode-api.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { config } from '../configs/auth.config';
+import { catchError } from 'rxjs/operators';
 
 export class XtreamCodeAPIService {
-    constructor(private http: HttpClient) { }
+    constructor(private httpClient: HttpClient, private router: Router) { }
 
-    get_liveStream_Category(){
-        
+    get_liveStream_Category( username: string, password: string){
+        const httpParams: HttpParams = new HttpParams().set('username', username).set('password', password).set('action', 'get_live_categories');
+        const endpoint = `${config.url}:${config.port}/player_api.php`;
+        return this.httpClient.get(`${endpoint}`, {params: httpParams});
+    }   
+
+    get_vodStream_Category(username: string, password: string){
+        const httpParams: HttpParams = new HttpParams().set('username', username).set('password', password).set('action', 'get_vod_categories');
+        const endpoint = `${config.url}:${config.port}/player_api.php`;
+        return this.httpClient.get(`${endpoint}`, {params: httpParams});
     }
 
-    get_vodStream_Category(){
-
-    }
-
-    get_seriesStream_Category(){
-
+    get_seriesStream_Category(username: string, password: string){
+        const httpParams: HttpParams = new HttpParams().set('username', username).set('password', password).set('action', 'get_series_categories');
+        const endpoint = `${config.url}:${config.port}/player_api.php`;
+        return this.httpClient.get(`${endpoint}`, {params: httpParams});
     }
     
 }
