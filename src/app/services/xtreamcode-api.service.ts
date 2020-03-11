8cea3ac01db84438580c0fb9283ce1bd9f234f8d
AndrLewis <lewis.andrew.byu@outlook.com>
2020-03-11 19:49:14 +0800

    Done XC API's live,vod,series' category.

---
+++ src/app/services/xtreamcode-api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { config } from '../configs/auth.config';
+
+export class XtreamCodeAPIService {
+    constructor(private http: HttpClient) { }
+
+    get_liveStream_Category(){
+        
+    }
+
+    get_vodStream_Category(){
+
+    }
+
+    get_seriesStream_Category(){
+
+    }
+    
+}
