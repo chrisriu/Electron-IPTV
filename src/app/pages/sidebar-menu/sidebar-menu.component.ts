854b60b8383bd4109316a302ded77b1b2c8b022c
AndrLewis <60868470+AndrLewis@users.noreply.github.com>
2020-03-24 01:53:03 +0800

    SubMenu Scroll of Six

--- src/app/pages/sidebar-menu/sidebar-menu.component.ts
+++ src/app/pages/sidebar-menu/sidebar-menu.component.ts
@@ -24,18 +24,12 @@ export class SidebarMenuComponent implements OnInit {
     ngOnInit(): void {
         (function ($) {
             $(".components a").on('click', function () {
-
                 // Case of the Selected collapse categories again
                 if(!$(this).parent('li').hasClass('active')){
                     // Collapse & Expand Menu of the Categories Progress...
                     $('.components').find('li.active').find('ul').removeClass('show')
                     $('.components').find('li.active').removeClass('active')
                     $(this).parent('li').addClass('active')
-
-                    // Show only the subcategories...
-
-                } else {   // Case of the Selected Expaned categories again
-                    
                 }
             });
         })(jQuery)
